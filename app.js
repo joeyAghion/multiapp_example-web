397ea1ca749d623f0d5fd5c84525f4452c575c80
Joey Aghion <joey@aghion.com>
2014-05-06 23:43:39 -0400

    Update example web app to request widget data from example API app and render it at /shop

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 var express = require('express'),
+http = require('http'),
 app = express(),
-port = process.env['PORT'] || 3000;
+port = process.env['PORT'] || 3000,
+api_url = process.env['API_URL'] || 'http://api.example.com';
 
 app.get('/', function(req, res) {
   res.send('<a href="/shop">Browse products</a>');
 });
 
-var server = app.listen(port, function() {
-    console.log('Listening on port %d', server.address().port);
+app.get('/shop', function(req, res) {
+  http.get(api_url + '/api/widgets', function(apiRes) {
+    var data = '';
+    apiRes.on('data', function(chunk) {
+      data += chunk;
+    });
+    apiRes.on('end', function() {
+      var obj = JSON.parse(data);
+      res.send(obj.map(function(widget) {
+        return "<p>" + widget.name + ", " + widget.price_cents + "</p>";
+      }).join("\n"));
+    });
+  }).on('error', function(e) {
+    res.send('Got error: ' + e.message);
+  });
 });
+
+app.listen(port);
