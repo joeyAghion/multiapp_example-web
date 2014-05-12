b37c80785177b228014362e654d7550d0b500730
Joey Aghion <joey@aghion.com>
2014-05-11 21:15:43 -0400

    refactor API helper

--- app.js
+++ app.js
@@ -2,26 +2,27 @@ var express = require('express'),
 http = require('http'),
 app = express(),
 port = process.env['PORT'] || 3000,
-api_url = process.env['API_URL'] || 'http://api.example.com';
+api_url = process.env['API_URL'] || 'http://api.example.com',
+
+getWidgets = function(callback, error) {
+  http.get(api_url + '/api/widgets', function(apiRes) {
+    var data = '';
+    apiRes.on('data', function(chunk) { data += chunk; });
+    apiRes.on('end', function() {
+      callback(data ? JSON.parse(data) : []);
+    });
+  }).on('error', function(e) { if (error) error(e); });
+};
 
 app.get('/', function(req, res) {
   res.send('<a href="/shop">Browse products</a>');
 });
 
 app.get('/shop', function(req, res) {
-  http.get(api_url + '/api/widgets', function(apiRes) {
-    var data = '';
-    apiRes.on('data', function(chunk) {
-      data += chunk;
-    });
-    apiRes.on('end', function() {
-      var obj = data ? JSON.parse(data) : [];
-      res.send(obj.map(function(widget) {
-        return "<p>" + widget.name + ", " + widget.price_cents + "</p>";
-      }).join("\n"));
-    });
-  }).on('error', function(e) {
-    res.send('Got error: ' + e.message);
+  getWidgets(function(data) {
+    res.send(data.map(function(widget) {
+      return "<p>" + widget.name + ", " + widget.price_cents + "</p>";
+    }).join("\n"));
   });
 });
 
